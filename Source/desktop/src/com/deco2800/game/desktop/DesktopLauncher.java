da73f1b073e6d0b275c3c5039b45b33b4a1b5c63
Aaron <thebelgiumesekid@gmail.com>
2021-03-18 17:02:06 +1000

    Skeleton libGDX project

---
+++ Source/desktop/src/com/deco2800/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.deco2800.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.deco2800.game.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new GdxGame(), config);
+	}
+}
