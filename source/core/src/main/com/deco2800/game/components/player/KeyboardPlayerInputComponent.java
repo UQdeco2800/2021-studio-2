95acd5617f4a5897051d576f0d22199619316517
Aaron <thebelgiumesekid@gmail.com>
2021-03-30 20:54:55 +1000

    Player moves with WASD

--- source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
@@ -1,12 +1,16 @@
 package com.deco2800.game.components.player;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
 import com.deco2800.game.input.InputComponent;
+import com.deco2800.game.math.Vector2Utils;
 
 /**
  * Input handler for the player for keyboard and touch (mouse) input.
  */
 public class KeyboardPlayerInputComponent extends InputComponent {
+  private final Vector2 walkDirection = Vector2.Zero.cpy();
 
   public KeyboardPlayerInputComponent () {
     this.setPriority(5);
@@ -19,13 +23,57 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   @Override
   public boolean keyDown(int keycode) {
     switch (keycode) {
-      case Input.Keys.A:
-        entity.getEvents().trigger("walk", 'u');
+      case Keys.W:
+        walkDirection.add(Vector2Utils.Up);
+        triggerWalkEvent();
         return true;
-      case Input.Keys.S:
+      case Keys.A:
+        walkDirection.add(Vector2Utils.Left);
+        triggerWalkEvent();
+        return true;
+      case Keys.S:
+        walkDirection.add(Vector2Utils.Down);
+        triggerWalkEvent();
+        return true;
+      case Keys.D:
+        walkDirection.add(Vector2Utils.Right);
+        triggerWalkEvent();
+        return true;
+      case Keys.SPACE:
         entity.getEvents().trigger("attack");
         return true;
     }
     return false;
+  }
+
+  @Override
+  public boolean keyUp(int keycode) {
+    switch (keycode) {
+      case Keys.W:
+        walkDirection.sub(Vector2Utils.Up);
+        triggerWalkEvent();
+        return true;
+      case Keys.A:
+        walkDirection.sub(Vector2Utils.Left);
+        triggerWalkEvent();
+        return true;
+      case Keys.S:
+        walkDirection.sub(Vector2Utils.Down);
+        triggerWalkEvent();
+        return true;
+      case Keys.D:
+        walkDirection.sub(Vector2Utils.Right);
+        triggerWalkEvent();
+        return true;
+    }
+      return false;
+  }
+
+  private void triggerWalkEvent() {
+    if (walkDirection.epsilonEquals(Vector2.Zero)) {
+      entity.getEvents().trigger("walkStop");
+    } else {
+      entity.getEvents().trigger("walk", walkDirection);
+    }
   }
 }
---
+++ source/core/src/main/com/deco2800/game/math/Vector2Utils.java
@@ -0,0 +1,11 @@
+package com.deco2800.game.math;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Vector2Utils {
+  public static Vector2 Left = new Vector2(-1f, 0f);
+  public static Vector2 Right = new Vector2(1f, 0f);
+  public static Vector2 Up = new Vector2(0f, 1f);
+  public static Vector2 Down = new Vector2(0f, -1f);
+  public static Vector2 One = new Vector2(1f, 1f);
+}
