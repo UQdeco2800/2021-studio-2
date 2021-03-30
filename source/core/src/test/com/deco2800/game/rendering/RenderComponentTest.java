7ac4f0bbbfc14506863a83d105a82b92cf63d231
Aaron <thebelgiumesekid@gmail.com>
2021-03-30 19:13:15 +1000

    Added tests

--- source/core/src/test/com/deco2800/game/rendering/RenderComponentTest.java
+++ source/core/src/test/com/deco2800/game/rendering/RenderComponentTest.java
@@ -1,9 +1,12 @@
 package com.deco2800.game.rendering;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
+import com.deco2800.game.entities.Entity;
 import com.deco2800.game.extensions.GameExtension;
 import com.deco2800.game.services.ServiceLocator;
 import org.junit.jupiter.api.Test;
@@ -39,5 +42,24 @@ class RenderComponentTest {
     RenderComponent component = spy(RenderComponent.class);
     component.render(null);
     verify(component).draw(any());
+  }
+
+  @Test
+  void shouldGiveCorrectRenderOrder() {
+    RenderComponent component1 = spy(RenderComponent.class);
+    RenderComponent component2 = spy(RenderComponent.class);
+    assertEquals(component1.getLayer(), component2.getLayer());
+
+    Entity entity1 = new Entity();
+    Entity entity2 = new Entity();
+    component1.setEntity(entity1);
+    component2.setEntity(entity2);
+
+    entity1.setPosition(0f, 1f);
+    entity2.setPosition(0f, 2f);
+    assertTrue(component1.getZIndex() > component2.getZIndex());
+
+    entity2.setPosition(5f, -3f);
+    assertTrue(component1.getZIndex() < component2.getZIndex());
   }
 }
---
+++ source/core/src/test/com/deco2800/game/utils/SortedIntMapTest.java
@@ -0,0 +1,43 @@
+package com.deco2800.game.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SortedIntMapTest {
+  @Test
+  void shouldPutGetCorrectly() {
+    SortedIntMap<Integer> map = new SortedIntMap<>(2);
+    map.put(3, 1);
+    map.put(0, -2);
+    assertEquals(1, map.get(3));
+    assertEquals(-2, map.get(0));
+    map.put(3, -4);
+    assertEquals(-4, map.get(3));
+    assertNull(map.get(10));
+  }
+
+  @Test
+  void shouldContains() {
+    SortedIntMap<Integer> map = new SortedIntMap<>(2);
+    assertFalse(map.containsKey(4));
+    assertFalse(map.contains(5));
+
+    map.put(2, 5);
+    assertTrue(map.contains(5));
+    assertTrue(map.containsKey(2));
+  }
+
+  @Test
+  void shouldClear() {
+    SortedIntMap<Integer> map = new SortedIntMap<>(2);
+    map.put(3, 1);
+    map.put(0, -2);
+    map.clear();
+    assertNull(map.get(3));
+    assertNull(map.get(0));
+  }
+}
