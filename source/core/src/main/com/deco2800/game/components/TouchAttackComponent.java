ea18343aa976579fcc73a8048de633b914c1deba
Aaron C <aaron.coox@outlook.com>
2021-05-01 20:11:47 +1000

    Comments and tests

--- source/core/src/main/com/deco2800/game/components/TouchAttackComponent.java
+++ source/core/src/main/com/deco2800/game/components/TouchAttackComponent.java
@@ -9,16 +9,33 @@ import com.deco2800.game.physics.PhysicsLayer;
 import com.deco2800.game.physics.components.HitboxComponent;
 import com.deco2800.game.physics.components.PhysicsComponent;
 
+/**
+ * When this entity touches a valid enemy's hitbox, deal damage to them and apply a knockback.
+ *
+ * <p>Requires CombatStatsComponent, HitboxComponent on this entity.
+ *
+ * <p>Damage is only applied if target entity has a CombatStatsComponent. Knockback is only applied
+ * if target entity has a PhysicsComponent.
+ */
 public class TouchAttackComponent extends Component {
   private short targetLayer;
   private float knockbackForce = 0f;
   private CombatStatsComponent combatStats;
   private HitboxComponent hitboxComponent;
 
+  /**
+   * Create a component which attacks entities on collision, without knockback.
+   * @param targetLayer The physics layer of the target's collider.
+   */
   public TouchAttackComponent(short targetLayer) {
     this.targetLayer = targetLayer;
   }
 
+  /**
+   * Create a component which attacks entities on collision, with knockback.
+   * @param targetLayer The physics layer of the target's collider.
+   * @param knockback The magnitude of the knockback applied to the entity.
+   */
   public TouchAttackComponent(short targetLayer, float knockback) {
     this.targetLayer = targetLayer;
     this.knockbackForce = knockback;
