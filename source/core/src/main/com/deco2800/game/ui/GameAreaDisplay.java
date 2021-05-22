da1961b1dd291c539e4897451f1b8ab435e7267d
Aaron C <aaron.coox@outlook.com>
2021-05-22 19:18:55 +1000

    updated UI to ui

---
+++ source/core/src/main/com/deco2800/game/ui/GameAreaDisplay.java
@@ -0,0 +1,40 @@
+package com.deco2800.game.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+public class GameAreaDisplay extends UIComponent {
+  private String gameAreaName = "";
+  private Label title;
+
+  public GameAreaDisplay(String gameAreaName) {
+    this.gameAreaName = gameAreaName;
+  }
+
+  @Override
+  public void create() {
+    super.create();
+    addActors();
+  }
+
+  private void addActors() {
+    title = new Label(this.gameAreaName, skin, "large");
+    stage.addActor(title);
+  }
+
+  @Override
+  public void draw(SpriteBatch batch)  {
+    int screenHeight = Gdx.graphics.getHeight();
+    float offsetX = 10f;
+    float offsetY = 30f;
+
+    title.setPosition(offsetX, screenHeight - offsetY);
+  }
+
+  @Override
+  public void dispose() {
+    super.dispose();
+    title.remove();
+  }
+}
---
+++ source/core/src/main/com/deco2800/game/ui/UIComponent.java
@@ -0,0 +1,41 @@
+package com.deco2800.game.ui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.deco2800.game.rendering.RenderComponent;
+import com.deco2800.game.rendering.Renderable;
+import com.deco2800.game.services.ServiceLocator;
+
+/** A generic component for rendering onto the ui. */
+public abstract class UIComponent extends RenderComponent implements Renderable {
+  private static final int UI_LAYER = 2;
+  protected static final Skin skin =
+      new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
+  protected Stage stage;
+
+  @Override
+  public void create() {
+    super.create();
+    stage = ServiceLocator.getRenderService().getStage();
+  }
+
+  @Override
+  public int getLayer() {
+    return UI_LAYER;
+  }
+
+  @Override
+  public float getZIndex() {
+    return 1f;
+  }
+
+  public Vector2 getCenteredPosition(Actor actor) {
+    int screenWidth = stage.getViewport().getScreenWidth();
+    int screenHeight = stage.getViewport().getScreenHeight();
+    return new Vector2(
+        (screenWidth - actor.getWidth()) / 2, (screenHeight - actor.getHeight()) / 2);
+  }
+}
