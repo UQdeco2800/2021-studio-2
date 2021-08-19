d979acf145a01fd18fe7746407265388a98c88d6
Jonathan Tang <jonoymtang@gmail.com>
2021-08-19 17:49:58 +1000

    Added constant walking speed on diagonal movement.

--- source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
@@ -6,12 +6,29 @@ import com.badlogic.gdx.math.Vector2;
 import com.deco2800.game.input.InputComponent;
 import com.deco2800.game.utils.math.Vector2Utils;
 
+//This class has been imported to allow for a short delay
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Input handler for the player for keyboard and touch (mouse) input.
  * This input handler only uses keyboard input.
  */
 public class KeyboardPlayerInputComponent extends InputComponent {
   private final Vector2 walkDirection = Vector2.Zero.cpy();
+
+  /** Distance scale for moving in a diagonal direction. */
+  public final float DIAGONAL_DISTANCE = 0.7071f;
+
+  private float up = 0f;
+
+  private float left = 0f;
+
+  private float down = 0f;
+
+  private float right = 0f;
+
+  private float speedMultiplier = 1;
 
   public KeyboardPlayerInputComponent() {
     super(5);
@@ -27,24 +44,39 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   public boolean keyDown(int keycode) {
     switch (keycode) {
       case Keys.W:
-        walkDirection.add(Vector2Utils.UP);
+        this.up = 1;
         triggerWalkEvent();
         return true;
       case Keys.A:
-        walkDirection.add(Vector2Utils.LEFT);
+        this.left = 1;
         triggerWalkEvent();
         return true;
       case Keys.S:
-        walkDirection.add(Vector2Utils.DOWN);
+        this.down = 1;
         triggerWalkEvent();
         return true;
       case Keys.D:
-        walkDirection.add(Vector2Utils.RIGHT);
+        this.right = 1;
         triggerWalkEvent();
         return true;
       case Keys.SPACE:
         entity.getEvents().trigger("attack");
         return true;
+      case Keys.SHIFT_LEFT:
+        this.speedMultiplier = 1.4f;
+        triggerWalkEvent();
+        return true;
+      case Keys.Q:
+        this.speedMultiplier = 2.5f;
+        triggerWalkEvent();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+          @Override
+          public void run() {
+            speedMultiplier = 1f;
+            triggerWalkEvent();
+          }
+        }, 150);
       default:
         return false;
     }
@@ -60,19 +92,23 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   public boolean keyUp(int keycode) {
     switch (keycode) {
       case Keys.W:
-        walkDirection.sub(Vector2Utils.UP);
+        this.up = 0;
         triggerWalkEvent();
         return true;
       case Keys.A:
-        walkDirection.sub(Vector2Utils.LEFT);
+        this.left = 0;
         triggerWalkEvent();
         return true;
       case Keys.S:
-        walkDirection.sub(Vector2Utils.DOWN);
+        this.down = 0;
         triggerWalkEvent();
         return true;
       case Keys.D:
-        walkDirection.sub(Vector2Utils.RIGHT);
+        this.right = 0;
+        triggerWalkEvent();
+        return true;
+      case Keys.SHIFT_LEFT:
+        this.speedMultiplier = 1;
         triggerWalkEvent();
         return true;
       default:
@@ -81,10 +117,105 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   }
 
   private void triggerWalkEvent() {
-    if (walkDirection.epsilonEquals(Vector2.Zero)) {
+    //Checks to see if the player should be static or is currently moving.
+    if ((this.up - this.down) == 0 && (this.right - this.left) == 0) {
       entity.getEvents().trigger("walkStop");
     } else {
+      float x = this.right - this.left;
+      float y = this.up - this.down;
+      if (x != 0 && y != 0) {
+        x = (this.right - this.left) * DIAGONAL_DISTANCE;
+        y = (this.up - this.down) * DIAGONAL_DISTANCE;
+      }
+      walkDirection.x = x * this.speedMultiplier;
+      walkDirection.y = y * this.speedMultiplier;
       entity.getEvents().trigger("walk", walkDirection);
+
     }
   }
 }
+
+
+// Original Code
+///**
+// * Input handler for the player for keyboard and touch (mouse) input.
+// * This input handler only uses keyboard input.
+// */
+//public class KeyboardPlayerInputComponent extends InputComponent {
+//  private final Vector2 walkDirection = Vector2.Zero.cpy();
+//
+//  public KeyboardPlayerInputComponent() {
+//    super(5);
+//  }
+//
+//  /**
+//   * Triggers player events on specific keycodes.
+//   *
+//   * @return whether the input was processed
+//   * @see InputProcessor#keyDown(int)
+//   */
+//  @Override
+//  public boolean keyDown(int keycode) {
+//    switch (keycode) {
+//      case Keys.W:
+//        walkDirection.add(Vector2Utils.UP);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.A:
+//        walkDirection.add(Vector2Utils.LEFT);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.S:
+//        walkDirection.add(Vector2Utils.DOWN);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.D:
+//        walkDirection.add(Vector2Utils.RIGHT);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.SPACE:
+//        entity.getEvents().trigger("attack");
+//        return true;
+//      default:
+//        return false;
+//    }
+//  }
+//
+//  /**
+//   * Triggers player events on specific keycodes.
+//   *
+//   * @return whether the input was processed
+//   * @see InputProcessor#keyUp(int)
+//   */
+//  @Override
+//  public boolean keyUp(int keycode) {
+//    switch (keycode) {
+//      case Keys.W:
+//        walkDirection.sub(Vector2Utils.UP);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.A:
+//        walkDirection.sub(Vector2Utils.LEFT);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.S:
+//        walkDirection.sub(Vector2Utils.DOWN);
+//        triggerWalkEvent();
+//        return true;
+//      case Keys.D:
+//        walkDirection.sub(Vector2Utils.RIGHT);
+//        triggerWalkEvent();
+//        return true;
+//      default:
+//        return false;
+//    }
+//  }
+//
+//  private void triggerWalkEvent() {
+//    if (walkDirection.epsilonEquals(Vector2.Zero)) {
+//      entity.getEvents().trigger("walkStop");
+//    } else {
+//      entity.getEvents().trigger("walk", walkDirection);
+//    }
+//  }
+//}
