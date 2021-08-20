f62b60f6ff1d560e73b919fe953e5b2c5cf21d2b
Jonathan Tang <jonoymtang@gmail.com>
2021-08-20 11:55:58 +1000

    Added a dash event function in PlayerActions class. Updated the dash to ignore movements during the dash. #49

--- source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/KeyboardPlayerInputComponent.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
 import com.deco2800.game.input.InputComponent;
-import com.deco2800.game.utils.math.Vector2Utils;
+import com.deco2800.game.services.ServiceLocator;
 
 //This class has been imported to allow for a short delay for dash abilities
 import java.util.Timer;
@@ -20,20 +20,29 @@ public class KeyboardPlayerInputComponent extends InputComponent {
   /** Distance scale for moving in a diagonal direction. */
   public final float DIAGONAL_DISTANCE = 0.7071f;
 
+  /** Multiplier difference for the dash ability. */
+  public final float DASH_MULTIPLIER = 2.5f;
+
   /** When the player is pressing W, up is 1, else, up is 0. */
-  private float up = 0f;
+  private byte up = 0;
 
   /** When the player is pressing A, up is 1, else, up is 0. */
-  private float left = 0f;
+  private byte left = 0;
 
   /** When the player is pressing S, up is 1, else, up is 0. */
-  private float down = 0f;
+  private byte down = 0;
 
   /** When the player is pressing D, up is 1, else, up is 0. */
-  private float right = 0f;
+  private byte right = 0;
+
+  /** While the player is in their dash, it is 1, else it is 0. */
+  private byte dashing = 0;
 
   /** Used to change the speed of the player quickly. */
   private float speedMultiplier = 1;
+
+  /** Stores the last system time since the dash ability was pressed.*/
+  private long lastDash = 0L;
 
   public KeyboardPlayerInputComponent() {
     super(5);
@@ -71,14 +80,16 @@ public class KeyboardPlayerInputComponent extends InputComponent {
         this.speedMultiplier = 1.4f;
         triggerWalkEvent();
         return true;
-      case Keys.Q:
-        this.speedMultiplier = 2.5f;
-        triggerWalkEvent();
+      case Keys.CAPS_LOCK:
+        dashing = 1;
+        triggerDashEvent();
+        update();
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
           @Override
           public void run() {
             speedMultiplier = 1f;
+            dashing = 0;
             triggerWalkEvent();
           }
         }, 150);
@@ -121,22 +132,49 @@ public class KeyboardPlayerInputComponent extends InputComponent {
     }
   }
 
+  /**
+   * Triggers the walk event for the player character. The method will handle
+   * diagonal movement to ensure that walk speed is consistent and check if
+   * the player is no longer moving.
+   */
   private void triggerWalkEvent() {
+    if (dashing == 1) {
+      return;
+    }
     //Checks to see if the player should be static or is currently moving.
     if ((this.up - this.down) == 0 && (this.right - this.left) == 0) {
       entity.getEvents().trigger("walkStop");
     } else {
-      float x = this.right - this.left;
-      float y = this.up - this.down;
-      if (x != 0 && y != 0) {
-        x = (this.right - this.left) * DIAGONAL_DISTANCE;
-        y = (this.up - this.down) * DIAGONAL_DISTANCE;
-      }
-      walkDirection.x = x * this.speedMultiplier;
-      walkDirection.y = y * this.speedMultiplier;
+      calculateDistance(speedMultiplier);
       entity.getEvents().trigger("walk", walkDirection);
-
     }
+  }
+
+  /**
+   * Triggers the dash event for the player character. The method will
+   * call a movement at a certain speed.
+   */
+  private void triggerDashEvent() {
+    calculateDistance(DASH_MULTIPLIER);
+    entity.getEvents().trigger("dash", walkDirection);
+  }
+
+  /**
+   * Helper function to set the X and Y coordinate movement distances.
+   * The method will calculate the distance and check if the player is moving
+   * in a diagonal movement and change the distances accordingly.
+   *
+   * @param multiplier multiplies the distance moved by the character
+   */
+  private void calculateDistance(float multiplier) {
+    float x = this.right - this.left;
+    float y = this.up - this.down;
+    if (x != 0 && y != 0) {
+      x = (float)(this.right - this.left) * DIAGONAL_DISTANCE;
+      y = (float)(this.up - this.down) * DIAGONAL_DISTANCE;
+    }
+    walkDirection.x = x * multiplier;
+    walkDirection.y = y * multiplier;
   }
 }
 
