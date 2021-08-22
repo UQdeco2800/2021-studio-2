8bfea64c3f16b7e97d604b09dceba41a6e3677b4
Minh Tieu <s4641971@uqconnect.edu.au>
2021-08-22 18:42:53 +1000

    arrow movement - arrow stop at mid air

---
+++ source/core/src/main/com/deco2800/game/components/npc/ArrowAnimationController.java
@@ -0,0 +1,28 @@
+package com.deco2800.game.components.npc;
+
+import com.deco2800.game.components.Component;
+import com.deco2800.game.rendering.AnimationRenderComponent;
+
+/**
+ * This class listens to events relevant to a ghost entity's state and plays the animation when one
+ * of the events is triggered.
+ */
+public class GhostAnimationController extends Component {
+  AnimationRenderComponent animator;
+
+  @Override
+  public void create() {
+    super.create();
+    animator = this.entity.getComponent(AnimationRenderComponent.class);
+    entity.getEvents().addListener("wanderStart", this::animateWander);
+    entity.getEvents().addListener("chaseStart", this::animateChase);
+  }
+
+  void animateWander() {
+    animator.startAnimation("float");
+  }
+
+  void animateChase() {
+    animator.startAnimation("angry_float");
+  }
+}
---
+++ source/core/src/main/com/deco2800/game/entities/factories/ArrowFactory.java
@@ -0,0 +1,2 @@
+package com.deco2800.game.entities.factories;public class ArrowFactory {
+}
