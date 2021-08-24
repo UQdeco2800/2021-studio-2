1b7fdcc1b10cfbde1cd6490a4545c43cde4661a9
Minh Tieu <s4641971@uqconnect.edu.au>
2021-08-24 21:16:22 +1000

    intial attempt on enemy trigger alert. Focus on update for all ghost entities

---
+++ source/core/src/main/com/deco2800/game/components/tasks/AlertTask.java
@@ -0,0 +1,103 @@
+package com.deco2800.game.components.tasks;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+import com.deco2800.game.ai.tasks.PriorityTask;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.services.ServiceLocator;
+
+/** Chases a target entity but stays far enough away so it can attack safely */
+public class RangedChaseTask extends ChaseTask implements PriorityTask {
+
+  /**
+   * @param target The entity to chase.
+   * @param priority Task priority when chasing (0 when not chasing).
+   * @param viewDistance Maximum distance from the entity at which chasing can start.
+   * @param maxChaseDistance Maximum distance from the entity while chasing before giving up.
+   */
+  public RangedChaseTask(Entity target, int priority, float viewDistance, float maxChaseDistance) {
+    super(target, priority, viewDistance, maxChaseDistance);
+  }
+
+  @Override
+  public void start() {
+    super.start();
+    movementTask = new MovementTask(calculatePos());
+    movementTask.create(owner);
+    movementTask.start();
+    //Deadzone
+    if (super.getDistanceToTarget() < owner.getEntity().getAttackRange() * 8/10) {
+      this.owner.getEntity().getEvents().trigger("wanderStart");
+    } else if (super.getDistanceToTarget() > owner.getEntity().getAttackRange()) {
+      //this.owner.getEntity().getEvents().trigger("fleeStart");
+      this.owner.getEntity().getEvents().trigger("chaseStart");
+    }
+
+  }
+
+
+  @Override
+  public void update() {
+    movementTask.setTarget(calculatePos());
+    movementTask.update();
+    if (movementTask.getStatus() != Status.ACTIVE) {
+      movementTask.start();
+    }
+  }
+
+  @Override
+  public int getPriority() {
+    //Deadzone
+    if (super.getDistanceToTarget() < owner.getEntity().getAttackRange() * 8/10) {
+      return (super.getPriority());
+    } else if (super.getDistanceToTarget() > owner.getEntity().getAttackRange()) {
+      return (super.getPriority());
+    }
+    return (-1);
+
+  }
+
+  /**
+   *
+   * @return position that is close enough to attack but staying as far away as possible
+   */
+  private Vector2 calculatePos() {
+    Vector2 v1 = owner.getEntity().getPosition().cpy();
+    Vector2 v2 = target.getPosition().cpy();
+    Vector2 v3 = v1.cpy().sub(v2); //heading relative to entity
+    float range = owner.getEntity().getAttackRange();
+    float distanceFrom = v3.len();
+    if (range/v3.len() < 1) {
+      v3.scl(range / v3.len());
+      v3.add(v2);
+    } else {
+      //invert v3 direction
+      v3 = v2.cpy().sub(v1);
+      v3.rotate90(1).rotate90(1); //heading relative to target
+      //v3.scl(v3.len() / range);
+      if (range/(v3.len()+distanceFrom) < 1) {
+        v3.scl(range / (v3.len()+distanceFrom));
+      }
+      v3.add(v1);
+    }
+    ServiceLocator.getRenderService().getDebug().drawLine(v1, v3, Color.RED, 2);
+    return v3;
+    /*
+    Vector2 v3 = v2.cpy().sub(v1); //heading relative to entity
+    float range = owner.getEntity().getAttackRange();
+    if (Math.abs(v3.x) > Math.abs(v3.y)) {
+      if (Math.abs(v3.x - range) < Math.abs(v3.x + range)) {
+        v3.x -= range;
+      } else {
+        v3.x += range;
+      }
+    } else {
+      if (Math.abs(v3.y - range) < Math.abs(v3.y + range)) {
+        v3.y -= range;
+      } else {
+        v3.y += range;
+      }
+    }
+    return v3.add(v1);*/
+  }
+}
