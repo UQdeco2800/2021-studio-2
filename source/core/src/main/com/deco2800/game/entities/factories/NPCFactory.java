3da223b0036e42542bc58ba8b674b840ed74c557
BobJohnsom <kai.barry1234@gmail.com>
2021-08-25 12:24:21 +1000

    increased knockback and added delay between enemy attacks

--- source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
@@ -212,7 +212,7 @@ public class NPCFactory {
             .addComponent(new PhysicsMovementComponent())
             .addComponent(new ColliderComponent())
             .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
-            .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 1.5f))
+            .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 2.5f))
             .addComponent(aiComponent);
 
     PhysicsUtils.setScaledCollider(npc, 0.9f, 0.4f);
@@ -231,7 +231,7 @@ public class NPCFactory {
             .addComponent(new PhysicsMovementComponent())
             .addComponent(new ColliderComponent())
             .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
-            .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 1.5f));
+            .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 2.5f));
 
     PhysicsUtils.setScaledCollider(npc, 0.9f, 0.4f);
     return npc;
