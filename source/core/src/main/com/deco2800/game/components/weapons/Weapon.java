1950bad2dec71829c15e8c23bd5712a2ff5f26ab
Jacob Edwards <jacob.edwards1@uqconnect.edu.au>
2021-08-27 20:01:21 +1000

    Changed weapon class. Modified weapon class to have protected variables, such that they can be used in subclasses. Also added create() function, and attack function to include attack direction. #13

--- source/core/src/main/com/deco2800/game/components/weapons/Weapon.java
+++ source/core/src/main/com/deco2800/game/components/weapons/Weapon.java
@@ -1,15 +1,18 @@
 package com.deco2800.game.components.weapons;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.components.Component;
 
 /**
- * Represents the overall weapon class for which all weapon-types will inherit from.
+ * Represents the overall weapon class for which all weapons will inherit from.
  */
 public abstract class Weapon extends Component {
-    private int attackPower; // damage
-    private short targetLayer; // weapon target, determines what entities the weapon can inflict damage upon.
-    private float knockback; // NOTE: optional
-
+    protected int attackPower; // damage
+    protected short targetLayer; // weapon target (what the weapon can damage)
+    protected float knockback;
+    protected CombatStatsComponent combatStats;
 
     public Weapon(short targetLayer, int attackPower, float knockback) {
         this.targetLayer = targetLayer;
@@ -17,6 +20,11 @@ public abstract class Weapon extends Component {
         this.knockback = knockback;
     }
 
-    public abstract void attack();
+    @Override
+    public void create() {
+        combatStats = entity.getComponent(CombatStatsComponent.class);
+    }
+
+    public abstract void attack(int attackDirection);
 
 }
