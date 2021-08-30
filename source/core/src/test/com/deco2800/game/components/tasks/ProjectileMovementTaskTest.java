603c64ebab21ba1186dab7f70ae929020e59d75c
Minh Tieu <s4641971@uqconnect.edu.au>
2021-08-30 16:07:17 +1000

    Add test for ProjectileMovementTas and ShootProjectTileTask. Need to work on ShootProjectileTaskTest for sprint 2.

--- source/core/src/test/com/deco2800/game/components/tasks/ProjectileMovementTaskTest.java
+++ source/core/src/test/com/deco2800/game/components/tasks/ProjectileMovementTaskTest.java
@@ -1,86 +1,87 @@
 package com.deco2800.game.components.tasks;
 
+import com.badlogic.gdx.math.Vector2;
+import com.deco2800.game.ai.tasks.AITaskComponent;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.physics.PhysicsService;
+import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.physics.components.PhysicsMovementComponent;
+import com.deco2800.game.rendering.DebugRenderer;
+import com.deco2800.game.rendering.RenderService;
+import com.deco2800.game.services.GameTime;
+import com.deco2800.game.services.ServiceLocator;
+import net.dermetfan.gdx.physics.box2d.PositionController;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProjectileMovementTaskTest {
 
     @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void start() {
-    }
-
-    @Test
-    void update() {
+    void beforeEach() {
+        RenderService renderService = new RenderService();
+        renderService.setDebug(mock(DebugRenderer.class));
+        ServiceLocator.registerRenderService(renderService);
+        GameTime gameTime = mock(GameTime.class);
+        when(gameTime.getDeltaTime()).thenReturn(20f / 1000);
+        ServiceLocator.registerTimeSource(gameTime);
+        ServiceLocator.registerPhysicsService(new PhysicsService());
     }
 
     @Test
-    void setTarget() {
+    void shouldMoveTowardsTarget() {
+        Entity target = new Entity();
+        target.setPosition(4f, 4f);
+
+        ProjectileMovementTask projectileMovementTask = new ProjectileMovementTask(target.getPosition(), new Vector2(4f, 4f));
+        AITaskComponent ai = new AITaskComponent().addTask(projectileMovementTask);
+        Entity entity = makePhysicsEntity().addComponent(ai);
+        entity.create();
+        entity.setPosition(0f, 0f);
+
+        float initialDistance = entity.getPosition().dst(target.getPosition());
+        // Run the game for a few cycles
+        for (int i = 0; i < 78; i++) {
+            entity.earlyUpdate();
+            entity.update();
+            ServiceLocator.getPhysicsService().getPhysics().update();
+        }
+        float newDistance = entity.getPosition().dst(target.getPosition());
+        assertTrue(newDistance < initialDistance);
+        // can't check because the item is already dispose - so does not exist
     }
 
     @Test
-    void setMoveSpeed() {
+    void shouldChaseOnlyWhenInDistance() {
+        Entity target = new Entity();
+        target.setPosition(6f, 6f);
+
+        Entity entity = makePhysicsEntity();
+        entity.create();
+        entity.setPosition(0f, 0f);
+
+        ProjectileMovementTask projectileMovementTask =
+                new ProjectileMovementTask(target.getPosition(), new Vector2(2f, 2f));
+        projectileMovementTask.create(() -> entity);
+
+        // Arrow should travel toward target
+        assertEquals(10, projectileMovementTask.getPriority());
+
+        projectileMovementTask.start();
+        entity.earlyUpdate();
+        entity.update();
+        // When active, should chase if within chase distance
+        assertEquals(10, projectileMovementTask.getPriority());
+
     }
 
-    @Test
-    void stop() {
-    }
-
-    @Test
-    void isAtTarget() {
-    }
-
-    @Test
-    void checkIfStuck() {
-    }
-
-    @Test
-    void didMove() {
-    }
-
-    @Test
-    void create() {
-    }
-
-    @Test
-    void testStart() {
-    }
-
-    @Test
-    void testUpdate() {
-    }
-
-    @Test
-    void testStop() {
-    }
-
-    @Test
-    void getStatus() {
-    }
-
-    @Test
-    void testUpdate1() {
-    }
-
-    @Test
-    void getPriority() {
-    }
-
-    @Test
-    void testStop1() {
-    }
-
-    @Test
-    void stoppedMoving() {
+    private Entity makePhysicsEntity() {
+        return new Entity()
+                .addComponent(new PhysicsComponent())
+                .addComponent(new PhysicsMovementComponent());
     }
 }
--- source/core/src/test/com/deco2800/game/components/tasks/ShootProjectileTaskTest.java
+++ source/core/src/test/com/deco2800/game/components/tasks/ShootProjectileTaskTest.java
@@ -1,74 +1,74 @@
 package com.deco2800.game.components.tasks;
 
+import com.deco2800.game.ai.tasks.AITaskComponent;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.physics.PhysicsService;
+import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.physics.components.PhysicsMovementComponent;
+import com.deco2800.game.rendering.DebugRenderer;
+import com.deco2800.game.rendering.RenderService;
+import com.deco2800.game.services.GameTime;
+import com.deco2800.game.services.ServiceLocator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ShootProjectileTaskTest {
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void create() {
-    }
-
-    @Test
-    void start() {
+        RenderService renderService = new RenderService();
+        renderService.setDebug(mock(DebugRenderer.class));
+        ServiceLocator.registerRenderService(renderService);
+        GameTime gameTime = mock(GameTime.class);
+        when(gameTime.getDeltaTime()).thenReturn(20f / 1000);
+        ServiceLocator.registerTimeSource(gameTime);
+        ServiceLocator.registerPhysicsService(new PhysicsService());
     }
 
     @Test
-    void update() {
+    void checkShootPossible() {
+        Entity target = new Entity();
+        target.setPosition(2f, 2f);
+
+        ShootProjectileTask shootProjectileTask = new ShootProjectileTask(target, 2000);
+        shootProjectileTask.setProjectileType("normalArrow");
+        shootProjectileTask.setMultishotChance(0.1);
+        AITaskComponent ai = new AITaskComponent().addTask(shootProjectileTask);
+        Entity entity = makePhysicsEntity().addComponent(ai);
+        entity.create();
+        entity.setAttackRange(5);
+        entity.setPosition(0f, 0f);
+        // can see enemy
+        assertEquals(20, shootProjectileTask.getPriority());
+        shootProjectileTask.start();
+        // need to wait 2 seconds (cool down) before can shoot
+        long time = System.currentTimeMillis();
+        while (System.currentTimeMillis() - time <= 2000) {
+            // Wait
+        }
+        // can see enemy
+        assertEquals(20, shootProjectileTask.getPriority());
+
+        // need to count the number of spawn entity
+        for (int i = 0; i < 5; i++) {
+            entity.earlyUpdate();
+            //entity.update();
+            ServiceLocator.getPhysicsService().getPhysics().update();
+        }
+
+        assertEquals(20, shootProjectileTask.getPriority());
     }
 
-    @Test
-    void stop() {
-    }
+    // not done - need to fine tune on edge cases (do for sprint 2)
 
-    @Test
-    void getStatus() {
-    }
-
-    @Test
-    void testStart() {
-    }
-
-    @Test
-    void testUpdate() {
-    }
-
-    @Test
-    void shoot() {
-    }
-
-    @Test
-    void setFollowTarget() {
-    }
-
-    @Test
-    void setShowTrajectory() {
-    }
-
-    @Test
-    void setMultishotChance() {
-    }
-
-    @Test
-    void setProjectileType() {
-    }
-
-    @Test
-    void testStop() {
-    }
-
-    @Test
-    void getPriority() {
+    private Entity makePhysicsEntity() {
+        return new Entity()
+                .addComponent(new PhysicsComponent())
+                .addComponent(new PhysicsMovementComponent());
     }
 }
