93931faea3e836f638ea7d68df44182b7d199e86
Eco9898 <eco9898@live.com>
2021-09-01 15:56:36 +1000

    checkpoint 1
    Collision boxes can now be rotated

--- source/core/src/main/com/deco2800/game/entities/Entity.java
+++ source/core/src/main/com/deco2800/game/entities/Entity.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.utils.IntMap;
 import com.deco2800.game.components.Component;
 import com.deco2800.game.components.ComponentType;
 import com.deco2800.game.events.EventHandler;
+import com.deco2800.game.physics.components.HitboxComponent;
 import com.deco2800.game.services.ServiceLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,6 +41,7 @@ public class Entity {
   private boolean disposeYourself = false;
   private float attackRange;
   private String entityType;
+  private float angle;
 
   public Entity() {
     id = nextId;
@@ -135,6 +137,19 @@ public class Entity {
    */
   public void setScale(Vector2 scale) {
     this.scale = scale.cpy();
+  }
+
+  public float getAngle() {
+    return angle;
+  }
+
+  /**
+   *
+   * @param angle to rotate in degrees
+   */
+  public void setAngle(float angle) {
+    this.angle = angle;
+    //this.getComponent(HitboxComponent.class).setAngle(angle);
   }
 
   /**
--- source/core/src/main/com/deco2800/game/rendering/TextureRenderComponent.java
+++ source/core/src/main/com/deco2800/game/rendering/TextureRenderComponent.java
@@ -14,7 +14,6 @@ import java.awt.*;
 public class TextureRenderComponent extends RenderComponent {
   private final Texture texture;
   private final Sprite sprite;
-  private float rotation = 0;
 
   /**
    * @param texturePath internal path of static teture to render. Will be scaled to the entity's
@@ -31,10 +30,9 @@ public class TextureRenderComponent extends RenderComponent {
   }
 
   /** @param sprite Sprite to render,  Will be scaled to the entity's scale. */
-  public TextureRenderComponent(Sprite sprite, float rotation) {
+  public TextureRenderComponent(Sprite sprite) {
     this.texture = null;
     this.sprite = sprite;
-    this.rotation = rotation;
   }
 
   /** Scale the entity to a width of 1 and a height matching the texture's ratio */
@@ -52,13 +50,14 @@ public class TextureRenderComponent extends RenderComponent {
     Vector2 positionCenter = entity.getCenterPosition();
     //Vector2 positionCenterRelative = positionCenter.cpy().sub(position);
     Vector2 scale = entity.getScale();
+    float angle = entity.getAngle();
 
     if (texture != null) {
       batch.draw(texture, position.x, position.y, scale.x, scale.y);
     } else if (sprite != null) {
-      Vector2 newScale = scale.cpy().rotateAroundDeg(new Vector2(0,0),  -rotation);
+      Vector2 newScale = scale.cpy().rotateAroundDeg(new Vector2(0,0),  -angle);
       //batch.draw(sprite, position.x, position.y, positionCenterRelative.x, positionCenterRelative.y, 1, 1, scale.x, scale.y, rotation);
-      batch.draw(sprite, position.x, position.y, 0, 0, 1, 1, newScale.x, newScale.y, rotation);
+      batch.draw(sprite, position.x, position.y, 0, 0, 1, 1, newScale.x, newScale.y, angle);
     }
   }
 }
