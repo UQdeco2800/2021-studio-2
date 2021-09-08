c2ea20b3a7d7b6bcf81ef5ae69748372090597e1
Eco9898 <eco9898@live.com>
2021-09-08 12:59:35 +1000

    Update WeaponFactory.java

--- source/core/src/main/com/deco2800/game/entities/factories/WeaponFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/WeaponFactory.java
@@ -69,6 +69,7 @@ public class WeaponFactory {
 
     public static Entity createTrackingArrow(Entity targetEntity, float angle) {
         Entity normalArrow = createBaseArrow();
+        normalArrow.setEntityType("fireball");
         TrackingArrowConfig config = configs.trackingArrow;
         ProjectileMovementTask movementTask = new ProjectileMovementTask(targetEntity, new Vector2(config.speedX, config.speedY));
         AITaskComponent aiComponent =
@@ -89,6 +90,7 @@ public class WeaponFactory {
 
     public static Entity createFastArrow(Vector2 targetLoc, float angle) {
         Entity normalArrow = createBaseArrow();
+        normalArrow.setEntityType("sniperArrow");
         FastArrowConfig config = configs.fastArrow;
         ProjectileMovementTask movementTask = new ProjectileMovementTask(targetLoc, new Vector2(config.speedX, config.speedY));
         AITaskComponent aiComponent =
