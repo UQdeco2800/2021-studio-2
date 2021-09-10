8bdcd6a788cdb764ce7ca62b9d341b2fdfeb11d8
GoldMan93 <jasonpsc@qq.com>
2021-09-10 18:50:00 +1000

    Deleting unnecessary code

--- source/core/src/main/com/deco2800/game/physics/PhysicsEngine.java
+++ source/core/src/main/com/deco2800/game/physics/PhysicsEngine.java
@@ -56,12 +56,10 @@ public class PhysicsEngine implements Disposable {
       world.step(PHYSICS_TIMESTEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
       accumulator -= PHYSICS_TIMESTEP;
     }
-    if (bodyList.size() != 0) {
-      for (int i = 0; i < bodyList.size(); i++) {
-        world.destroyBody(bodyList.get(i));
-      }
-      bodyList.clear();
+    for (int i = 0; i < bodyList.size(); i++) {
+      world.destroyBody(bodyList.get(i));
     }
+    bodyList.clear();
   }
 
   public Body createBody(BodyDef bodyDef) {
