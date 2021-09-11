12c2378c2c8988548d4ba36c437847607e4c7027
eco9898 <ecoprograms01@gmail.com>
2021-09-11 14:17:49 +1000

    Automated Style fixes

--- source/core/src/main/com/deco2800/game/entities/LineEntity.java
+++ source/core/src/main/com/deco2800/game/entities/LineEntity.java
@@ -1,16 +1,15 @@
 package com.deco2800.game.entities;
 
 import com.badlogic.gdx.math.Vector2;
-import com.deco2800.game.rendering.TextureRenderComponent;
 
 /**
  * Creates an entity that represents a line.
  */
 public class LineEntity extends Entity {
 
-    private int xOffset = 0;
     /**
      * draws a line entity from origin to target
+     *
      * @param target location of target
      * @param origin location of owner
      */
@@ -19,7 +18,7 @@ public class LineEntity extends Entity {
         Vector2 v2 = target.cpy();
         Vector2 v3 = v2.cpy().sub(v1);
         this.setAngle(v3.angleDeg());
-        Vector2 centerPoint = v3.cpy().setLength(v3.len()/2).add(v1);
+        Vector2 centerPoint = v3.cpy().setLength(v3.len() / 2).add(v1);
         Vector2 bodyOffset = this.getCenterPosition().cpy().sub(this.getPosition());
         Vector2 position = centerPoint.sub(bodyOffset);
         this.setPosition(position);
