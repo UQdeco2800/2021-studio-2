47a9f209fe4346c3bcd270b1e0f320c0b59bbc69
r-xian <rodxiang2@gmail.com>
2021-09-11 17:00:26 +1000

    changed heal amount to a fix amount

--- source/core/src/main/com/deco2800/game/components/TouchHealComponent.java
+++ source/core/src/main/com/deco2800/game/components/TouchHealComponent.java
@@ -9,9 +9,6 @@ import com.deco2800.game.physics.PhysicsLayer;
  * component to allow entities to give health to entities on collision
  */
 public class TouchHealComponent extends TouchComponent {
-    /** the percentage of maxHP to restore health by */
-    private float healthPercentage = 0.8f;
-
     /**
      * Create a component which allows Player entity to interact with the Health item and give itself health
      *
@@ -39,11 +36,8 @@ public class TouchHealComponent extends TouchComponent {
             return;
         }
         if (enabled) {
-            //heal the player 10% of max hp
             Entity player = ((BodyUserData) other.getBody().getUserData()).entity;
-            float healthRestore =
-                    player.getComponent(CombatStatsComponent.class).getMaxHealth() * healthPercentage;
-            player.getEvents().trigger("healEntity", (int) healthRestore);
+            player.getEvents().trigger("healEntity", 100);
             //dispose health potion after giving player hp
             ((BodyUserData) me.getBody().getUserData()).entity.prepareDispose();
         }
