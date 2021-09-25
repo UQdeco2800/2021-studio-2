0cbad41f9cca8f3696c48c984ad4e47bf9dd8cdd
Jacob Edwards <jacob.edwards1@uqconnect.edu.au>
2021-09-25 14:31:14 +1000

    Added longsword, #254

---
+++ source/core/src/main/com/deco2800/game/components/weapons/Longsword.java
@@ -0,0 +1,9 @@
+package com.deco2800.game.components.weapons;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Longsword extends MeleeWeapon {
+    public Longsword(short targetLayer, int attackPower, float knockback, Vector2 weaponSize) {
+        super(targetLayer, attackPower, knockback, weaponSize);
+    }
+}
