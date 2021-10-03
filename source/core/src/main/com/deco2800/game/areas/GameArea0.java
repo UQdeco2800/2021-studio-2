4ce3d70dbc70fc25c04681f8ff81a6b18031cee5
Jonathan Tang <jonoymtang@gmail.com>
2021-10-04 02:16:54 +1000

    Fixed issues with incorrect function names.

--- source/core/src/main/com/deco2800/game/areas/GameArea0.java
+++ source/core/src/main/com/deco2800/game/areas/GameArea0.java
@@ -224,7 +224,7 @@ public class GameArea0 extends GameArea {
     }
 
     private void spawnTeleport() {
-        Entity teleport = ObstacleFactory.creatTeleport();
+        Entity teleport = ObstacleFactory.createTeleport();
         GridPoint2 fixedPos = new GridPoint2(15, 10);
         spawnEntityAt(teleport, fixedPos, true, true);
         //boss= 1;
