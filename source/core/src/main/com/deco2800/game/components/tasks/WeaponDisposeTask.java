91a41b6c2547bea168c71991fc500e81739f99a1
Minh Tieu <s4641971@uqconnect.edu.au>
2021-10-04 17:48:11 +1000

    update style (doc comment

--- source/core/src/main/com/deco2800/game/components/tasks/WeaponDisposeTask.java
+++ source/core/src/main/com/deco2800/game/components/tasks/WeaponDisposeTask.java
@@ -10,13 +10,25 @@ import com.deco2800.game.rendering.TextureRenderComponent;
 import com.deco2800.game.services.GameTime;
 import com.deco2800.game.services.ServiceLocator;
 
+/**
+ * Wait for the weapon break animation to run
+ */
 public class WeaponDisposeTask extends ProjectileMovementTask implements PriorityTask {
+    /** Wait duration */
     private final float duration;
+    /** Start time */
     private double start;
+    /** Start end */
     private boolean declareEnd;
+    /** Time */
     private final GameTime timeSource;
 
-
+    /**
+     *  Pause the weapon in midair to wait for animation to run
+     * @param targetLoc target position
+     * @param speed speed
+     * @param duration pause duration
+     */
     public WeaponDisposeTask(Vector2 targetLoc, Vector2 speed, float duration) {
         super(targetLoc, speed);
         this.duration = duration;
@@ -24,11 +36,17 @@ public class WeaponDisposeTask extends ProjectileMovementTask implements Priorit
         this.declareEnd = true;
     }
 
+    /**
+     * call wait for death animation
+     */
     @Override
     public void update() {
         waitForDeathAnimation();
     }
 
+    /**
+     * Dispose weapon hit box and wait for the weapon break animation to run
+     */
     public void waitForDeathAnimation() {
         if (this.declareEnd) {
             this.start = System.currentTimeMillis();
@@ -44,9 +62,12 @@ public class WeaponDisposeTask extends ProjectileMovementTask implements Priorit
         }
     }
 
+    /**
+     * override all other task if the weapon is dead (health = 0);
+     * @return 100 if weapon is dead, 0, otherwise
+     */
     @Override
     public int getPriority() {
-//        return 0;
         if (owner.getEntity().getComponent(CombatStatsComponent.class).isDead()) {
             return 100;
         } else {
