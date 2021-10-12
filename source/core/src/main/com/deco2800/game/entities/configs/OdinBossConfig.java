16d26ebd873c89fe529cbe7481b1bd235e37336b
r-xian <rodxiang2@gmail.com>
2021-10-12 20:38:09 +1000

    minor comment changes

--- source/core/src/main/com/deco2800/game/entities/configs/OdinBossConfig.java
+++ source/core/src/main/com/deco2800/game/entities/configs/OdinBossConfig.java
@@ -4,6 +4,6 @@ package com.deco2800.game.entities.configs;
  * Defines the properties stored in the odin config files to be loaded by the NPC Factory.
  */
 public class OdinBossConfig extends MeleeEnemyConfig {
-    public int health = 300; //good luck
+    public int health = 10; //good luck
     public int attack = 100; //try and not facetank this dude
 }
