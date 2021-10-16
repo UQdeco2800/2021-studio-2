4621da0e04e942734f707809eaaac32cc1855ec2
eco9898 <ecoprograms01@gmail.com>
2021-10-16 14:55:49 +1000

    New fireball circle collider

--- source/core/src/main/com/deco2800/game/entities/factories/WeaponFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/WeaponFactory.java
@@ -179,7 +179,11 @@ public class WeaponFactory {
                                 new Vector2(config.speedX, config.speedY), 0.8f));
         fireBall.data.put("fireBallMovement", false);
 
-        ColliderComponent hitbox = new HitboxComponent().setLayer(PhysicsLayer.IDLEPROJECTILEWEAPON);
+        CircleShape circle = new CircleShape();
+        circle.setRadius(0.25f);
+        circle.setPosition(circle.getPosition().add(new Vector2(1,1).scl(0.5f)));
+        ColliderComponent hitbox = new HitboxComponent().setLayer(PhysicsLayer.IDLEPROJECTILEWEAPON)
+                .setShape(circle).setRestitution(0);
 
         fireBall
                 .addComponent(animator)
@@ -192,7 +196,6 @@ public class WeaponFactory {
                 .addComponent(new PlayerActions())
                 .addComponent(new TouchAttackComponent(PhysicsLayer.NONE, 1f));
         shootingSound("fireBall");
-        hitbox.setScale(0.8f);
         return fireBall;
     }
 
