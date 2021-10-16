52fb66d29e18efbd4325486f9c0ad2fc273ed4b4
r-xian <rodxiang2@gmail.com>
2021-10-16 17:21:45 +1000

    deleted odinConfig and we are using the elfBossConfig

--- source/core/src/main/com/deco2800/game/entities/configs/OdinBossConfig.java
+++
@@ -1,9 +0,0 @@
-package com.deco2800.game.entities.configs;
-
-/**
- * Defines the properties stored in the odin config files to be loaded by the NPC Factory.
- */
-public class OdinBossConfig {
-    public int health = 10; //good luck
-    public int attack = 100; //try and not facetank this dude
-}
