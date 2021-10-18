ef0d7a67437ee5f477334c08aaabf35c7ebd6426
BenjaminGonzalez <georgeocode110@gmail.com>
2021-10-18 21:39:52 +1000

    Updated Background tile to one of known id "1123" to debug rendering issue

--- source/core/assets/maps/RawTiled/tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset2" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="../../Tileset/tileset2.png" width="768" height="768"/>
-</tileset>
