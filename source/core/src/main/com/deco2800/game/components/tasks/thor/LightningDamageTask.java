4e246ef1e6c471315f7843bb7444cd784f943773
martinrbaer <martinrbaer@gmail.com>
2021-10-19 13:58:10 +1000

    added lightning attack tasks

--- source/core/src/main/com/deco2800/game/components/tasks/thor/LightningDamageTask.java
+++ source/core/src/main/com/deco2800/game/components/tasks/thor/LightningDamageTask.java
@@ -1,2 +1,40 @@
-package com.deco2800.game.components.tasks.thor;public class LightningDamageTask {
+package com.deco2800.game.components.tasks.thor;
+
+import com.deco2800.game.ai.tasks.DefaultTask;
+import com.deco2800.game.ai.tasks.PriorityTask;
+import com.deco2800.game.services.ServiceLocator;
+
+import com.deco2800.game.ai.tasks.DefaultTask;
+import com.deco2800.game.ai.tasks.PriorityTask;
+import com.deco2800.game.services.ServiceLocator;
+
+/**
+ * Move to a given position, finishing when you get close enough. Requires an entity with a
+ * PhysicsMovementComponent. Entity will be disposed of after reaching its destination.
+ */
+public class LightningDamageTask extends DefaultTask implements PriorityTask {
+
+    /**
+     * Spawn time of the entity.
+     */
+    private final long spawnTime;
+
+    public LightningDamageTask() {
+        this.spawnTime = ServiceLocator.getTimeSource().getTime();
+    }
+
+    /**
+     * return the priority of the arrow
+     * If arrow is in moving, return 10, else return -1 and dispose the arrow
+     *
+     * @return int 10 if arrow is moving, -1 if arrow is not
+     */
+    public int getPriority() {
+        if ((ServiceLocator.getTimeSource().getTime() - spawnTime) > 1000) {
+            owner.getEntity().prepareDispose();
+            return -1;
+        }
+        return 10;
+    }
+
 }
