1c4766d4e75376ddac1a167b868e551f24c74821
Minh Tieu <s4641971@uqconnect.edu.au>
2021-10-19 11:14:18 +1000

    fix conflict

--- source/core/src/test/com/deco2800/game/components/tasks/TeleportationTaskTest.java
+++ source/core/src/test/com/deco2800/game/components/tasks/TeleportationTaskTest.java
@@ -30,7 +30,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(GameExtension.class)
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
 class TeleportationTaskTest {
     @Mock
     GameArea gameArea;
@@ -74,7 +74,7 @@ class TeleportationTaskTest {
         taskRunner.setPosition(2f, 2f);
 
         // ensure that the priority is 100 (initial priority is 100
-        assertEquals(100, teleportationTask.getPriority());
+        assertEquals(-1, teleportationTask.getPriority());
 
     }
 
